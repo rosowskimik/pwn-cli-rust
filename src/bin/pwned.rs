a407fd9fde9567ff1ca77934d49b17d49045ff32
Mikołaj Rosowski <m.rosowski1@wp.pl>
2020-07-18 17:54:59 +0200

    Finished

--- src/bin/pwned.rs
+++ src/bin/pwned.rs
@@ -1,7 +1,25 @@
-use pwned::{hash, read};
+use pwned::{hash, pass, request};
+use std::process;
 
 fn main() {
-    let password = read::read_password();
+    let password = pass::read_password();
     let hashed_password = hash::hash_string(password);
-    dbg!(hashed_password);
+    let (head, tail) = hashed_password.split_at(5);
+
+    let response_list = match request::search_head(head) {
+        Ok(response) => response.text().unwrap(),
+        Err(_) => {
+            eprintln!("Check your internet connection");
+            process::exit(1);
+        }
+    };
+
+    match pass::search_pass(tail, &response_list) {
+        Some(count) => {
+            println!("Your password was found {} times.", count);
+        }
+        None => {
+            println!("Your password is safe.");
+        }
+    }
 }
